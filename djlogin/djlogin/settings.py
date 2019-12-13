86b8b84facb9aae06f29fa0109e7e041464234eb
baljnyam <baljnyam@gmail.com>
2019-12-14 02:26:43 +1100

    All-merges

--- djlogin/djlogin/settings.py
+++ djlogin/djlogin/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'userperm.apps.UserpermConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
